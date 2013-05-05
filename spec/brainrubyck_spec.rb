fdd137062825659b661bed0e135d2c9f1960364e
Guilherme Berger <gulherme.berger@gmail.com>
2013-05-04 21:02:32 -0300

    use expect instead of should in the spec

--- spec/brainrubyck_spec.rb
+++ spec/brainrubyck_spec.rb
@@ -7,74 +7,74 @@ describe BrainRubyck do
 
   describe ".new" do
     it "returns a new BrainRubyck object with defaults" do
-      br.code.should eql ""
-      br.logging.should eql false
-      br.dump_all.should eql false
+      expect(br.code).to eq ""
+      expect(br.logging).to eq false
+      expect(br.dump_all).to eq false
     end
   end
 
   describe "#code" do
     it "is empty on initialization" do
-      br.code.should eql ""
+      expect(br.code).to eq ""
     end
     it "is accessible" do
       br.code = "+++"
-      br.code.should eql "+++"
+      expect(br.code).to eq "+++"
     end
   end
 
   describe "#parse!" do
     it "returns the output" do
-      br.parse!.should_not be_nil
+      expect(br.parse!).not_to be_nil
     end
     it "return has length equal to the number of . commands" do
       br.code = "+++...+++...+++...>>>...<<<..."
-      br.parse!.length.should eql 15
+      expect(br.parse!.length).to eq 15
     end
     it 'returns \'Hello World!\n\' when using the Hello World program' do
       br.code = hello_world_code
-      br.parse!.should eql "Hello World!\n" 
+      expect(br.parse!).to eq "Hello World!\n" 
     end
     it 'outputs \'Hello World!\n\' to STDOUT when using the Hello World program' do
       output = capture_stdout do 
         br.code = hello_world_code
         br.parse!
       end
-      output.should eql "Hello World!\n"
+      expect(output).to eq "Hello World!\n"
     end
     it "logs the output to @out" do
       br.code = '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.'
       br.parse!
-      br.out.should eql "Hello World!\n"
+      expect(br.out).to eq "Hello World!\n"
     end
     it "starts the cursor at 0" do
       br.code = ""
-      br.cursor.should eql 0
+      expect(br.cursor).to eq 0
     end
     it "increments the current memory position with +" do
       br.code = "+"
       br.parse!
-      br.memory[br.cursor].should eql 1
+      expect(br.memory[br.cursor]).to eq 1
     end
     it "decrements the current memory position with -" do
       br.code = "-"
       br.parse!
-      br.memory[br.cursor].should eql -1
+      expect(br.memory[br.cursor]).to eq -1
     end
     it "increments the cursor with >" do
       br.code = ">"
       br.parse!
-      br.cursor.should eql 1
+      expect(br.cursor).to eq 1
     end
     it "decrements the cursor with <" do
       br.code = "<"
       br.parse!
-      br.cursor.should eql -1
+      expect(br.cursor).to eq -1
     end
     it "loops while current value != 0 with [ and ]" do
       br.code = "++[-]"
       br.parse!
-      br.memory[br.cursor].should eql 0
+      expect(br.memory[br.cursor]).to eq 0
     end
   end
 
